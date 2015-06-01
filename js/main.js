8c1ea00605726ec4a5dea8b2a100fa5cbab5d759
Riley Porter <rileymp2@cs.washington.edu>
2015-05-31 17:35:40 -0700

    changed regional svg to force layout

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const birthYear = 1967;
 const presentYear = 2015;
 const scrollSensitivity = 10.0; // higher equals less sensitive
 var prevYear = birthYear;
-var currentYear = 2000;
+var currentYear = birthYear;
 
 var zoom = d3.behavior.zoom()
   .scaleExtent([1,(presentYear - birthYear) / scrollSensitivity + 1])
@@ -26,10 +26,13 @@ var artistNodes, artistLinks, artistMap;
 
 var isZoomed = false;
 
-var force, node, link;
-
 width = $(window).width();
 height = $(window).height();
+
+var force = d3.layout.force()
+    .size([width, height])
+    .on("tick", tick);
+var node, link;
 
 var projection = d3.geo.albersUsa()
   .scale(width)  // determines initial map size
@@ -46,47 +49,21 @@ function init(error) {
   setupMap();
   drawMap();
   drawSlider();
-  setRegions();
-  updateRegionalForce();
+  createRegions();
+  setUpRegions();
 }
 
-
-function updateRegionalForce() {
-  force = d3.layout.force()
-    .nodes(regionNodes)
-    .start();
-
-  node = svg.selectAll(".node")
-    .data(regionNodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag)
-    .append("svg:circle")
-      .attr("r", function(d) { 
-        calculateArtists(d);
-        return d.numArtists;
-      })
-      .style("fill", function(d) {
-        return d.color;
-      })
-      .on("click", function() {
-        zoomToRegion(this); 
-      });
-
-  force.on("tick", function() {
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
+// redraws everything
+function redraw() {
+  width = $(window).width();
+  height = $(window).height();
+  d3.select('svg').remove();
+  setup();
+  drawMap();
+  updateRegions();
 }
 
-function calculateArtists(node) {
-  // total up the numArtists up to the current year and set numArtists attribute accordingly
-  for (var year in node.artistsPerYear) {
-    if (parseInt(year) <= currentYear) {
-      node.numArtists += parseInt(node.artistsPerYear[year]);
-    }
-  }
-}
+// ======= Functions to create the Map ======= 
 
 // creates the svg
 function setupMap() {
@@ -110,13 +87,169 @@ function setupMap() {
     .style("fill", "rgba(0,0,0,0)");
 
   mapTranslateLeft = $(window).width() * 0.1;
-  regionsGroup = svg.append("g").attr("id", "regions");
   g.attr("transform", "translate(" + (-1 * mapTranslateLeft) + ",0)");
-  regionsGroup.attr("transform", "translate(" + (-1 * mapTranslateLeft) + ",0)");
 
   // setup narrator box and get timeline events ready
   narrationSetup();
 }
+
+// draws the map in the group 'g' (this must be initialized before calling this function)
+function drawMap() {
+  d3.json("data/us.json", function(error, us) {
+    // remove alaska and hawaii
+    us.objects.states.geometries = us.objects.states.geometries.filter(
+      function(state) { 
+        return state.id != 2 && state.id != 15; 
+      }
+    );
+    g.append("g")
+      .attr("id", "states")
+    .selectAll("path")
+      .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  g.append("path")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("id", "state-borders")
+      .attr("d", path);
+  });
+}
+
+// ======= Functions to create and modify the region nodes ======= 
+
+// sets up and draws the region circles
+function createRegions() {
+  regionNodes.forEach(function(regionNode) {
+    var lon = regionNode.lon;
+    var lat = regionNode.lat;
+    var regionX = projection([-1*lon, lat])[0];
+    var regionY = projection([-1*lon, lat])[1];
+    regionNode["x"] = regionX;
+    regionNode["y"] = regionY;
+    regionNode["fixed"] = true;
+  });
+}
+
+function setUpRegions() {
+  force.nodes(regionNodes);
+
+  node = svg.selectAll(".node")
+    .data(regionNodes)
+    .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag)
+    .append("svg:circle")
+      .style("fill", function(d) {
+        return d.color;
+      })
+      .on("click", function(d) {
+        zoomToRegion(d); 
+      });
+
+  force.start();
+
+}
+
+function updateRegions() {
+  regionNodes.forEach(function(d){
+    calculateArtists(d);
+  });
+
+  d3.selectAll("circle").attr("r", function(d) { 
+    return Math.max(0, 10 * Math.log(d.numArtists)); 
+  });
+}
+
+function hideRegions() {
+  d3.selectAll("circle").attr("r", 0);
+}
+
+function tick() {
+  node.attr("cx", function(d) { return d.x - mapTranslateLeft; })
+      .attr("cy", function(d) { return d.y; });
+}
+
+function calculateArtists(node) {
+  node.numArtists = 0;
+  // total up the numArtists up to the current year and set numArtists attribute accordingly
+  for (var year in node.artistsPerYear) {
+    if (parseInt(year) <= currentYear) {
+      node.numArtists += parseInt(node.artistsPerYear[year]);
+    }
+  }
+}
+
+
+// ======= Functions to create and modify the slider ======= 
+
+function drawSlider() {
+  slider = d3.slider().axis(d3.svg.axis().orient("left").tickFormat(d3.format("d")).tickValues([1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015]))
+                      .value(1967).min(1967).max(2015).orientation("vertical").step(1);
+  slider.on("slide", function(evt, value) {
+    moveThroughTime(value);
+  });
+  d3.select("#slider").style("left", narrationWidth + "px")
+                      .style("top", $(window).height() * 0.05 + "px")
+                      .style("height", height-$(window).height() * 0.15 + "px").call(slider)
+                      .style("border", "0px");
+}
+
+// called when the user scrolls to zoom, moves through years from 1967 to 2015
+function moveThroughTime(newYear) {
+  if (newYear === undefined) {
+    currentYear = Math.round((d3.event.scale - 1) * scrollSensitivity + birthYear);
+    slider.value(currentYear);
+  } else {
+    currentYear = newYear;
+  }
+  updateRegions();
+  updateNarration();
+}
+
+var throttleTimer;
+function throttle() {
+  window.clearTimeout(throttleTimer);
+    throttleTimer = window.setTimeout(function() {
+      redraw();
+    }, 200);
+}
+
+// ======= Functions to handle zooming in and out of regions ======= 
+
+function zoomToRegion(region) {
+  var x, y, k;
+  var lon = region.lon;
+  var lat = region.lat;
+  x = projection([-1*lon, lat])[0];
+  y = projection([-1*lon, lat])[1];
+  k = region.scale;
+  hideRegions();
+  zoom.on("zoom", null);
+  g.on("dblclick", zoomOut);
+  g.transition()
+    .duration(750)
+    .attr("transform", "translate(" + (width - mapTranslateLeft) / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+    .style("stroke-width", 1.5 / k + "px");
+}
+
+function zoomOut() {
+  x = width / 2;
+  y = height / 2;
+  k = 1;
+  setUpRegions();
+  g.transition()
+    .duration(750)
+    .attr("transform", "translate(" + (width / 2 - mapTranslateLeft) + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+    .style("stroke-width", 1.5 / k + "px")
+    .each("end", function() { 
+      zoom.on("zoom", moveThroughTime); 
+      updateRegions();
+    });
+}
+
+
+// ======= Functions for managing the data ======= 
 
 function parseData() {
   var q = queue();
@@ -182,113 +315,3 @@ function parseData() {
   q.awaitAll(init);
 }
 
-// sets up and draws the region circles
-function setRegions() {
-  regionNodes.forEach(function(regionNode) {
-    var lon = regionNode.lon;
-    var lat = regionNode.lat;
-    var regionX = projection([-1*lon, lat])[0];
-    var regionY = projection([-1*lon, lat])[1];
-    regionNode["x"] = regionX;
-    regionNode["y"] = regionY;
-    regionNode["fixed"] = true;
-  });
-}
-
-
-function drawSlider() {
-  slider = d3.slider().axis(d3.svg.axis().orient("left").tickFormat(d3.format("d")).tickValues([1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015]))
-                      .value(1967).min(1967).max(2015).orientation("vertical").step(1);
-  slider.on("slide", function(evt, value) {
-    moveThroughTime(value);
-  });
-  d3.select("#slider").style("left", narrationWidth + "px")
-                      .style("top", $(window).height() * 0.05 + "px")
-                      .style("height", height-$(window).height() * 0.15 + "px").call(slider)
-                      .style("border", "0px");
-}
-
-// draws the map in the group 'g' (this must be initialized before calling this function)
-function drawMap() {
-  d3.json("data/us.json", function(error, us) {
-    // remove alaska and hawaii
-    us.objects.states.geometries = us.objects.states.geometries.filter(
-      function(state) { 
-        return state.id != 2 && state.id != 15; 
-      }
-    );
-    g.append("g")
-      .attr("id", "states")
-    .selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path);
-
-  g.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("id", "state-borders")
-      .attr("d", path);
-  });
-}
-
-// redraws everything
-function redraw() {
-  width = $(window).width();
-  height = $(window).height();
-  d3.select('svg').remove();
-  setup();
-  drawMap();
-  updateRegionalForce();
-}
-
-function zoomToRegion(region) {
-  var x, y, k;
-  console.log(region.id);
-  var lon = regions[region.id]["lon"];
-  var lat = regions[region.id]["lat"];
-  x = projection([-1*lon, lat])[0];
-  y = projection([-1*lon, lat])[1];
-  k = regions[region.id]["scale"];
-  d3.select("#regions").style("display", "none");
-  zoom.on("zoom", null);
-  g.on("dblclick", zoomOut);
-  g.transition()
-    .duration(750)
-    .attr("transform", "translate(" + (width - mapTranslateLeft) / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-    .style("stroke-width", 1.5 / k + "px");
-}
-
-function zoomOut() {
-  x = width / 2;
-  y = height / 2;
-  k = 1;
-  d3.select("#regions").style("display", "block");
-  g.transition()
-    .duration(750)
-    .attr("transform", "translate(" + (width / 2 - mapTranslateLeft) + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-    .style("stroke-width", 1.5 / k + "px")
-    .each("end", function() { 
-      zoom.on("zoom", moveThroughTime); 
-      drawRappers();
-    });
-}
-
-// called when the user scrolls to zoom, moves through years from 1967 to 2015
-function moveThroughTime(newYear) {
-  if (newYear === undefined) {
-    currentYear = Math.round((d3.event.scale - 1) * scrollSensitivity + birthYear);
-    slider.value(currentYear);
-  } else {
-    currentYear = newYear;
-  }
-  updateRegionalForce();
-  updateNarration();
-}
-
-var throttleTimer;
-function throttle() {
-  window.clearTimeout(throttleTimer);
-    throttleTimer = window.setTimeout(function() {
-      redraw();
-    }, 200);
-}
